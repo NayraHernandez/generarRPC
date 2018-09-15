edeaaaf402862cd68bad654cc11e09e5a8ffad2f
NayraHernandez <15390229@itchetumal.edu.mx>
2018-09-14 19:07:11 -0500

    Interfaz principal terminada.Inicio de programación

--- app/src/main/java/mx/edu/itchetumal/dadm/generarrpc/MainActivity.java
+++ app/src/main/java/mx/edu/itchetumal/dadm/generarrpc/MainActivity.java
@@ -1,7 +1,11 @@
 package mx.edu.itchetumal.dadm.generarrpc;
 
+import android.support.v4.widget.TextViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void GeneraRFC (View v){
+        //Tomar la primer letra y primer vocal del apellido paterno
+       EditText editA_Paterno = (EditText)findViewById(R.id.editA_Paterno);
+        char primerLetraAP = editA_Paterno.getText().charAt(0);
+
+        char[] vocales = {'A','E','I','O','U','a','e','i','o','u'};
+       String cadAux= "Pedro Pérez";
+       char primerLetra = cadAux.charAt(0);
+       Character LetraAp = new Character(cadAux.CharArt(0));
+      // LetraAp.compareTo()
+
+        //Tomar la primer letra del apellido materno
+
+        //Tomar la primer letra del primer nombre
+
+        //Tomar los ultimos digitos del año de nacimiento
+
+        //Tomar los dos digitos del mes de nacimiento
+
+        //Tomar los dos digitos del dia de nacimiento
+
+        //Generar tres caracteres (letras mayusculas o números) para la homonimia
+
+        //Concatenar lo anterior y mostrar el RFC
     }
 }
