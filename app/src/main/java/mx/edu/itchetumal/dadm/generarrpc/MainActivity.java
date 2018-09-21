05cf0ab2c2c6a19c503a43fdf2176d2cfd04eb0d
NayraHernandez <15390229@itchetumal.edu.mx>
2018-09-21 13:55:56 -0500

    Funciones para Ap_Paterno

--- app/src/main/java/mx/edu/itchetumal/dadm/generarrpc/MainActivity.java
+++ app/src/main/java/mx/edu/itchetumal/dadm/generarrpc/MainActivity.java
@@ -1,11 +1,13 @@
 package mx.edu.itchetumal.dadm.generarrpc;
 
-import android.support.v4.widget.TextViewCompat;
+
+import android.app.DatePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,29 +16,63 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    public void GeneraRFC (View v){
-        //Tomar la primer letra y primer vocal del apellido paterno
-       EditText editA_Paterno = (EditText)findViewById(R.id.editA_Paterno);
-        char primerLetraAP = editA_Paterno.getText().charAt(0);
+    public String adecuarCadena(String cadena){
+        String cadAux = cadena.toUpperCase().trim();
 
-        char[] vocales = {'A','E','I','O','U','a','e','i','o','u'};
-       String cadAux= "Pedro Pérez";
-       char primerLetra = cadAux.charAt(0);
-       Character LetraAp = new Character(cadAux.CharArt(0));
-      // LetraAp.compareTo()
+        char[] vocalesAcentuadas = {'Á', 'É', 'Í', 'Ó', 'Ú'};
+        char[] vocales = {'A', 'E', 'I', 'O', 'U'};
 
-        //Tomar la primer letra del apellido materno
+        for (byte pos=0; pos<appaterno.length(); pos++) {
+            cadAux.replace(vocalesAcentuadas[pos], vocales[pos]);
 
-        //Tomar la primer letra del primer nombre
+        }
+        return cadAux;
+    }
+    public void GeneraRFC(View v) {
+        //Tomar la primer letra apellido paterno
+        EditText editApPaterno = (EditText) findViewById(R.id.editA_Paterno);
 
-        //Tomar los ultimos digitos del año de nacimiento
+        String appterno = adecuarCadena(editApPaterno.getText().toString());
 
-        //Tomar los dos digitos del mes de nacimiento
+        Toast.makeText(this, "cadena convertida", Toast.LENGTH_LONG).show();
 
-        //Tomar los dos digitos del dia de nacimiento
+        char primerLetraAP = appaterno.charAt(0);
 
-        //Generar tres caracteres (letras mayusculas o números) para la homonimia
+        char[] vocales = {'A', 'E', 'I', 'O', 'U'};
+        char primerVocal = ' ';
+        byte pos;
+        boolean encontrada = false;
 
-        //Concatenar lo anterior y mostrar el RFC
+        for (pos=0; pos<vocales.length(); pos++) {
+            if (appterno.indexOf(vocales[pos]) >= 0) {
+                encontrada = true;
+                break;
+            }
+            if (encontrada)
+                primerVocal = vocales[pos];
+        }
+        //Hacer un método que reciba cadena y la devuelve en mayusculas,sin espacios al inicio o fin, sin acentos
+
+        //2. Tomar la primer letra del apellido materno
+        EditText editA_Materno = (EditText)findViewById(R.id.editA_Materno);
+        char primerLetraAM = editA_Materno.getText().charAt(0);
+
+        //3. Tomar la primer letra del primer nombre
+        EditText editNombre = (EditText)findViewById(R.id.editNombre);
+        char primerLetraNom = editNombre.getText().charAt(0);
+
+        //4. Tomar los ultimos digitos del año de nacimiento
+
+
+        //5. Tomar los dos digitos del mes de nacimiento
+
+        //6. Tomar los dos digitos del dia de nacimiento
+
+
+        //7. Generar tres caracteres (letras mayusculas o números) para la homonimia
+
+        //8. Concatenar lo anterior y mostrar el RFC
     }
 }
+
+
